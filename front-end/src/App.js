b66b6837b373d58ec8372ff664f1a42c7d260cc9
JordanRD <jordan.rafelino@gmail.com>
2021-03-16 11:50:28 +0700

    mengembalikan folder yg hilang

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -3,11 +3,12 @@ import { Route, Switch } from "react-router-dom"
 import Navigation from './components/navigation'
 import Register from './pages/register'
 import { useDispatch } from 'react-redux'
-import Home from './pages/home'
+// import Home from './pages/home'
 import Verification from './pages/verification'
 
 import { keepLogin } from './actions'
 import Cart from './pages/cart'
+import Login from './pages/login'
 import ForgotPasswordPage from './pages/forgotPasswordPage'
 
 const App = () => {
@@ -23,9 +24,10 @@ const App = () => {
       <Navigation />
       <Switch>
         <Route path='/register' component={Register} />
-        <Route path='/' component={Home} exact />
+        {/* <Route path='/' component={Home} exact /> */}
         <Route path='/cart' component={Cart}  />
         <Route path='/forgot' component={ForgotPasswordPage}  />
+        <Route path='/login' component={Login}  />
         <Route path='/verification' component={Verification} exact />
       </Switch>
     </div>
