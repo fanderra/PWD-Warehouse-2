9d32d29e68798d96ff9e87bc56cdbc93820fd898
JordanRD <jordan.rafelino@gmail.com>
2021-03-16 09:19:50 +0700

    forgot password

--- back-end/routers/userRouter.js
+++ back-end/routers/userRouter.js
@@ -23,9 +23,32 @@ const registerValidation = [
         .withMessage('Email is invalid')
 ]
 
+const editValidator= [
+    body('username')
+        .optional()
+        .matches(/\w{4,}/)
+        .withMessage('username can not contain spaces or symbol min 4 characters'),
+    body('email')
+        .optional()
+        .isEmail()
+        .withMessage('invalid email'),
+    body('password')
+        .optional()
+        .match(/.{6,}/)
+        .withMessage('password length must be at least 6 characters')
+        .match(/[a-z]/)
+        .withMessage('password must contain lowercase')
+        .match(/[A-Z]/)
+        .withMessage('password must contain uppercase')
+        .match(/\S/)
+        .withMessage('password can not contain spaces')
+]
+
 router.post('/register', registerValidation, userController.register)
 router.post('/keepLogin', verifyToken, userController.keepLogin)
 router.post('/showAll', userController.showAll)
 router.post('/login', userController.login)
+router.post('/forgot', userController.forgotPassword)
+router.patch('/reset',editValidator, verifyToken, userController.resetPassword)
 
 module.exports = router
--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React,{useEffect} from 'react'
 import { Route, Switch } from "react-router-dom"
 import Navigation from './components/navigation'
 import Register from './pages/register'
@@ -6,17 +6,25 @@ import { useDispatch } from 'react-redux'
 import Home from './pages/home'
 
 import { keepLogin } from './actions'
+import Cart from './pages/cart'
+import ForgotPasswordPage from './pages/forgotPasswordPage'
 
 const App = () => {
   const dispatch = useDispatch()
-  dispatch(keepLogin())
+  useEffect(() => {
+    if (localStorage.token) {
+      dispatch(keepLogin())
+    }
+  },[])
   
   return (
     <div>
       <Navigation />
       <Switch>
-        <Route path='/register' component={Register} exact />
+        <Route path='/register' component={Register} />
         <Route path='/' component={Home} exact />
+        <Route path='/cart' component={Cart}  />
+        <Route path='/forgot' component={ForgotPasswordPage}  />
       </Switch>
     </div>
   )
