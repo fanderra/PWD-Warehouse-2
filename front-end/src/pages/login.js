ddf611b9705fe39fe4d55a75e8b06f9bb7c6382c
fardnaer <rfpoerba@gmail.com>
2021-03-26 03:25:13 +0700

    PW2-1 Login

--- front-end/src/pages/login.js
+++ front-end/src/pages/login.js
@@ -5,55 +5,46 @@ import { Link, Redirect } from 'react-router-dom'
 import { login } from '../actions'
 
 const Login = () => {
-    const [username, setUsername] = React.useState('')
-    const [email, setEmail] = React.useState('')
-    const [password, setPassword] = React.useState('')
+    const [loginDetails, setLoginDetails] = React.useState({ username: '', email: '', password: '' })
     const [passVis, setPassVis] = React.useState(false)
-    const [logAlert, setLogAlert] = React.useState([false, ''])
-    const [logSuccess, setLogSuccess] = React.useState(false)
+    const [modalAlert, setModalAlert] = React.useState([false, ''])
+    
+    const dispatch = useDispatch()
+    const handleLog = () => {
+        if (!loginDetails.username && !loginDetails.password) return setModalAlert([true, 'Enter username/email & password'])
+        if (!loginDetails.username || !loginDetails.email) return setModalAlert([true, 'Enter username or email'])
+        if (!loginDetails.password) return setModalAlert([true, 'Enter password'])
+        
+        const user = { username: loginDetails.username, email: loginDetails.email, password: loginDetails.password }
+        dispatch(login(user, err => setModalAlert([true, err])))
+    }
     
     const { name } = useSelector((state) => {
         return {
             name: state.user.username
         }
     })
-    
-    const dispatch = useDispatch()
-    const handleLog = () => {
-        if (!username && !password) return setLogAlert([true, 'Enter your username/email & password'])
-        if (!username || !email) return setLogAlert([true, 'Enter your username or email'])
-        if (!password) return setLogAlert([true, 'Enter your password'])
-        
-        const user = { username, email, password }
-        dispatch(login(user, err => setLogAlert([true, err])))
-    }
-    if (name) return <Redirect to="/product" />
+    if (name) return <Redirect to="/" />
     
     return (
-        <div style={{ textAlign: "right" }}>
-            <Form style={{ width: "300px", marginLeft: "750px", position: "fixed" }}>
+        <div style={{ textAlign: "center", display: "flex", justifyContent: "center" }}>
+            <Form style={{ width: "300px", padding: 20, border: "1px solid black", borderRadius: 5 }}>
                 <h1>Login</h1>
-                <Form.Control onChange={event => {setUsername(event.target.value); setEmail(event.target.value)}} placeholder="Enter username or email" />
+                <Form.Control onChange={event => setLoginDetails({ ...loginDetails, username: event.target.value, email: event.target.value })} placeholder="Enter username or email" />
                 <InputGroup>
-                    <Form.Control onChange={event => setPassword(event.target.value)} placeholder="Enter password" type={passVis ? "text" : "password"} />
+                    <Form.Control onChange={event => setLoginDetails({ ...loginDetails, password: event.target.value })} placeholder="Enter password" type={passVis ? "text" : "password"} />
                     <InputGroup.Text onClick={() => setPassVis(!passVis)}>{passVis ? "HIDE" : "SHOW"}</InputGroup.Text>
                 </InputGroup>
-                <Button as={Link} to="/forgot">Forgot Password</Button>
-                <Button onClick={handleLog}>Submit</Button>
+                <Button variant="outline-info" onClick={handleLog}>Submit</Button>
+                <div>
+                    <Button variant="outline-info" as={Link} to="/forgotpassword">Forgot Password</Button>
+                    <Button variant="outline-info" as={Link} to="/register">Register</Button>
+                </div>
             </Form>
-            <Modal show={logAlert[0]} onHide={() => setLogAlert([false, ""])}>
-                <Modal.Header><Modal.Title>Alert</Modal.Title></Modal.Header>
-                <Modal.Body>{logAlert[1]}</Modal.Body>
-                <Modal.Footer>
-                    <Button onClick={() => setLogAlert([false, ""])}>Close</Button>
-                </Modal.Footer>
-            </Modal>
-            <Modal show={logSuccess} onHide={() => setLogSuccess(false)}>
-                <Modal.Header><Modal.Title>Alert</Modal.Title></Modal.Header>
-                <Modal.Body>Log In Successful!</Modal.Body>
-                <Modal.Footer>
-                    <Button as={Link} to="/product">Close</Button>
-                </Modal.Footer>
+            <Modal show={modalAlert[0]} onHide={() => setModalAlert([false, ""])} style={{ marginTop: 280 }}>
+                <Button variant="transparent" onClick={() => setModalAlert([false, ""])}>
+                    <Modal.Body>{modalAlert[1]}</Modal.Body>
+                </Button>
             </Modal>
         </div>
     )
